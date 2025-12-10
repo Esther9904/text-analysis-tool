2d9a941b3c527f42fb514e6ce7118ec47d1a2a1e
Esther9904 <taiwoibeh@gmail.com>
2025-12-09 20:54:45 -0800

    Added part of speech to lemmatizer

--- analyze.py
+++ analyze.py
@@ -1,6 +1,10 @@
 from random_username.generate import generate_username 
+import nltk
 from nltk.tokenize import word_tokenize,sent_tokenize 
 from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import wordnet
+nltk.download ('wordnet')
+nltk.download('averaged_perceptron_tagger_eng')
 wordLemmatizer = WordNetLemmatizer()
 import re
 
@@ -73,15 +77,32 @@ def getWordsPerSentence(sentences):
         totalWords += len(sentence.split(" "))
     return totalWords / len(sentences)
 
-# Filter raw tokenized words list to only include
-# valid english words 
-def cleanseWordList(words):
+# Convert part of speech from pos_tag() function
+#into wordnet compatible pos tag
+posToWordnetTag = {
+    "J": wordnet.ADJ,
+    "V": wordnet.VERB,
+    "N": wordnet.NOUN,
+    "R": wordnet.ADV
+}
+
+def treebankPosToWordnet(partOfSpeech):
+    posfirstChar = partOfSpeech[0]
+    if posfirstChar in posToWordnetTag:
+        return posToWordnetTag[posfirstChar] 
+    return wordnet.NOUN
+
+# Convert raw list of (word, POS) tuple to a list of strings
+# that only include valid english words 
+def cleanseWordList(posTaggedWordTuples):
     cleanseWords = []
     invalidWordPattern = "[^a-zA-Z-+]"
-    for word in words:
+    for posTaggedWordTuple in posTaggedWordTuples:
+        word = posTaggedWordTuple[0]
+        pos = posTaggedWordTuple[1]
         cleansedWord = word.replace(".","").lower()
         if (not re.search(invalidWordPattern, cleansedWord)) and len(cleansedWord) > 1:
-            cleanseWords.append(wordLemmatizer.lemmatize(cleansedWord))
+            cleanseWords.append(wordLemmatizer.lemmatize(cleansedWord,treebankPosToWordnet(pos)))
     return cleanseWords
 
 # Get User Details
@@ -100,7 +121,9 @@ keySentences = extractKeySentences(articleSentences,stockSearchPattern)
 wordsPerSentence = getWordsPerSentence(articleSentences)
 
 # Get word Analytics
-articleWordsCleansed = cleanseWordList(articleWords)
+wordsPosTagged = nltk.pos_tag(articleWords)
+articleWordsCleansed = cleanseWordList(wordsPosTagged)
+
 
 # Print for testing
 print("GOT:")
